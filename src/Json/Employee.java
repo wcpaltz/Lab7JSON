dbd4e169b5172d3cedc065a6259662a377da054d
Will Paltz <blizzard@Wills-MacBook-Pro.local>
2017-03-08 16:42:52 -0600

    Me and Pipedaddy’s work, BOOM

--- src/Json/Employee.java
+++ src/Json/Employee.java
@@ -1,5 +1,20 @@
 package Json;
 
 public class Employee {
-
+	private String LastName;
+	private String FirstName;
+	private String Phone;
+	private String Department;
+	
+	public Employee(String lName, String fName, String phone, String department){
+		this.LastName = lName;
+		this.FirstName = fName;
+		this.Phone = phone;
+		this.Department = department;
+	}
+	
+	public String toString(){
+		return "";
+		
+	}
 }
